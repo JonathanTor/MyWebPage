6fefba54ef46c090da4186fb1ac1f300b4582a1d
JonathanTor <98237664+JonathanTor@users.noreply.github.com>
2022-06-16 15:54:55 -0500

    Mi primera pagina web
    my web page

---
+++ app.js
@@ -0,0 +1,37 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+var check=document.querySelector(".check");
+check.addEventListener('click', idioma)
+
+function idioma() {
+	let id=check.checked;
+	if(id==true){
+		location.href="es.index.html";
+	}else{
+		location.href="index.html";
+	}
+}
+
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
